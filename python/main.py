77974e4fbedc77dd3430c93829a232db7cf52868
Philip Mateescu <github@philipm.at>
2018-05-28 17:33:43 -0500

    stats collector extracted to own file

--- python/main.py
+++ python/main.py
@@ -1,38 +1,8 @@
 import gzip
 import sys
 from xml.sax import make_parser
-from parser import LabelsHandler
-
-
-# http://www.discogs.com/help/voting-guidelines.html
-DATA_QUALITY_VALUES = (
-    'Needs Vote',
-    'Complete And Correct',
-    'Correct',
-    'Needs Minor Changes',
-    'Needs Major Changes',
-    'Entirely Incorrect',
-    'Entirely Incorrect Edit'
-)
-
-
-class StatsCollector:
-    def __init__(self):
-        global DATA_QUALITY_VALUES
-        self._data_quality = {dq: 0 for dq in DATA_QUALITY_VALUES}
-
-    def collect(self, model):
-        if model.id == 0:
-            return
-        quality = model.data_quality
-        if quality in self._data_quality:
-            self._data_quality[quality] += 1
-
-    def __str__(self):
-        # print(self._data_quality)
-        totals = sum(self._data_quality.values())
-        lines = "\n".join([f"{key.ljust(25)} = {value: >10,}" for key, value in self._data_quality.items()])
-        return f"Total: {totals} entries.\n{lines}"
+from handlers import LabelsHandler
+import stats
 
 
 def make_handler(file: str, collector):
@@ -51,7 +21,7 @@ def parse(file: str, parser) -> None:
 
 def main(argv):
     file = argv[0]
-    collector = StatsCollector()
+    collector = stats.StatsCollector()
     handler = make_handler(file, collector)
     if handler is None:
         print(f"I don't know how to handle {file}.")
---
+++ python/stats.py
@@ -0,0 +1,33 @@
+# http://www.discogs.com/help/voting-guidelines.html
+DATA_QUALITY_VALUES = (
+    'Needs Vote',
+    'Complete and Correct',
+    'Correct',
+    'Needs Minor Changes',
+    'Needs Major Changes',
+    'Entirely Incorrect',
+    'Entirely Incorrect Edit'
+)
+
+
+class StatsCollector:
+    def __init__(self):
+        global DATA_QUALITY_VALUES
+        self._data_quality = {dq: 0 for dq in DATA_QUALITY_VALUES}
+        self._keymap = {dq.lower():dq for dq in DATA_QUALITY_VALUES}
+
+    def collect(self, model):
+        if model.id == 0:
+            return
+        quality = model.data_quality
+        if quality.lower() in self._keymap:
+            key = self._keymap[quality.lower()]
+            self._data_quality[key] = self._data_quality[key] + 1
+        else:
+            print(f"Did not find quality: '{quality}' (id={model.id}). Keys: {self._data_quality.keys()}")
+
+    def __str__(self):
+        # print(self._data_quality)
+        totals = sum(self._data_quality.values())
+        lines = "\n".join(sorted([f"{key.ljust(25)} = {value: >10,}" for key, value in self._data_quality.items()]))
+        return f"Total: {totals} entries.\n{lines}"
